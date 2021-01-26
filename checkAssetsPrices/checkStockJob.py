ab4551ca7d56b83e81728ade71c45f5822ff2181
Pedro Jardim <drope.jardim@gmail.com>
2021-01-26 10:54:01 -0300

    Fix init project with no table error

--- checkAssetsPrices/checkStockJob.py
+++ checkAssetsPrices/checkStockJob.py
@@ -2,14 +2,19 @@ from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from checkAssetsPrices import checkAssetsPricesAPI
 from assetMonitoring.models import Schedule 
+import sys
 
 scheduler = BackgroundScheduler()
 
 def start():
-    schedule = Schedule.objects.last()
-    if schedule:
-        scheduler.add_job(checkAssetsPricesAPI.start_check_assets_prices, 'interval', minutes=schedule.minutes, id='check-prices')
-        scheduler.start()
+    try:
+        schedule = Schedule.objects.last()
+        if schedule:
+            scheduler.add_job(checkAssetsPricesAPI.start_check_assets_prices, 'interval', minutes=schedule.minutes, id='check-prices')
+            scheduler.start()
+    except:
+        e = sys.exc_info()[0]
+        print(e)
 
 def re_start():
     schedule = Schedule.objects.last()
